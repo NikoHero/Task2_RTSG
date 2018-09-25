73fea7cd6e469c24611b11cb10c08b64ec2e3d11
Nick <33632340+NikoHero@users.noreply.github.com>
2018-09-25 23:48:38 +0200

    Add files via upload

---
+++ Task2/17601321-Gade-A1/Building.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17601321_Gade_A1
+{
+    class Building
+    {
+        protected int XPosition;
+        protected int YPosition;
+        protected int Health;
+        protected string team;
+        protected string Symbol;
+
+        public Building()
+         {
+
+
+         }
+
+        public abstract void Death();
+        public abstract override string ToString();
+
+
+
+
+    }
+}
---
+++ Task2/17601321-Gade-A1/Unit.cs
@@ -0,0 +1,33 @@
+﻿namespace _17601321_Gade_A1
+{
+    //17601321 Nicholas Statheros
+    //Unique code : Sazu0
+    class Unit
+    {
+        protected int XPosition;
+        protected int YPosition;
+        protected int Health;
+        protected int MaxHealth;
+        protected int Speed;
+        protected int Attack;
+        protected int AttackRange;
+        protected string team;
+        protected string Symbol;
+        protected int IsAttacking;
+
+        protected string Name;
+        public Unit ()
+        {
+
+        }
+
+        public abstract void NewPosition();
+        public abstract void Combat();
+        public abstract void RangeAttack();
+        public abstract Unit ReturnPosition();
+        public abstract void Death();
+        public abstract override string ToString(); 
+
+
+    }
+}
